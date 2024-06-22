34a340d3de56f800641c48106bf3f26ad8ab25cc
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-06-22 13:34:01 -0300

    colocando criação de token

--- src/controllers/LoginUser.js
+++ src/controllers/LoginUser.js
@@ -1,10 +1,13 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 async function loginUser(req, res) {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+    const SECRET = 'itapecurutools'; 
 
     if (!user) {
       return res.status(404).json({ message: 'Usuário não encontrado.' });
@@ -16,11 +19,31 @@ async function loginUser(req, res) {
       return res.status(401).json({ message: 'Senha incorreta.' });
     }
 
-    res.status(200).json({ message: 'Login bem-sucedido!' });
+      const token = jwt.sign({userId: 1}, SECRET, {expiresIn: 600 });
+    res.status(200).json({ message: 'Login bem-sucedido!', user: {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      gender: user.gender,
+      ageRange: user.ageRange,
+      token
+    } });
   } catch (error) {
     console.error('Erro ao fazer login:', error);
     res.status(500).json({ message: 'Erro interno do servidor.' });
   }
 }
 
-export { loginUser };
+function verifyJWT(req, res, next){
+  const token = req.headers['x-access-token'];
+  jwt.verify(token, SECRET, (err, decoded) => {
+  if(err) return res.status(401).end();
+
+  req.userId = decoded.userId;
+  next();
+  })
+}
+
+export { loginUser, verifyJWT};
+
+
